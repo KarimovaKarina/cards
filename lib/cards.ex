555f20173b756a66d3539a911de0ff4d2b5503d8
Karina Karimova <kkarim@ttu.ee>
2020-11-17 16:33:08 +0200

    new fucntion create_hand and pipe operator

--- lib/cards.ex
+++ lib/cards.ex
@@ -28,12 +28,20 @@ defmodule Cards do
   end  
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "That file does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
     end  
+  end
+  
+  def create_hand(hand_size) do
+    #deck = Cards.create_deck
+    #deck = Cards.shuffle(deck)
+    #hand = Cards.deal(deck, hand_size)
+
+    Cards.create_deck 
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end  
 
 end
